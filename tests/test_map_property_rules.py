71fe54a839823548fee890bf72511d99a313ba81
{akebono03} <{shin8gou@gmail.com}>
2026-09-01 22:52:57 +0900

    phase28-4

--- tests/test_map_property_rules.py
+++ tests/test_map_property_rules.py
@@ -1,4 +1,6 @@
 from expression import (
+  HomotopyElement,
+  MapApplication,
   MapSymbol,
 )
 from map_property_rules import (
@@ -10,6 +12,8 @@ from proof import (
   InferenceTerminationReason,
   ProofRule,
   ProofStep,
+  Relation,
+  RelationType,
   apply_inference_match,
   find_inference_match,
   run_inference_until_stable_with_history,
@@ -294,6 +298,158 @@ def test_phase28_3_injectivity_is_not_derived_without_isomorphism():
 
   assert match is None
 
+
+def test_phase28_4_map_application_represents_f_of_a():
+  f = MapSymbol(
+    name="f",
+  )
+
+  a = HomotopyElement(
+    name="a",
+    dimension=1,
+  )
+
+  application = MapApplication(
+    map=f,
+    expression=a,
+  )
+
+  assert application.map == f
+  assert application.expression == a
+
+
+def test_phase28_4_map_application_equality_represents_f_a_equals_f_b():
+  f = MapSymbol(
+    name="f",
+  )
+
+  a = HomotopyElement(
+    name="a",
+    dimension=1,
+  )
+
+  b = HomotopyElement(
+    name="b",
+    dimension=1,
+  )
+
+  relation = Relation(
+    lhs=MapApplication(
+      map=f,
+      expression=a,
+    ),
+    rhs=MapApplication(
+      map=f,
+      expression=b,
+    ),
+    relation_type=RelationType.EQUALITY,
+  )
+
+  assert isinstance(
+    relation.lhs,
+    MapApplication,
+  )
+
+  assert isinstance(
+    relation.rhs,
+    MapApplication,
+  )
+
+  assert relation.lhs.map == f
+  assert relation.rhs.map == f
+
+  assert relation.lhs.expression == a
+  assert relation.rhs.expression == b
+
+  assert relation.relation_type == (
+    RelationType.EQUALITY
+  )
+
+
+def test_phase28_4_map_application_equality_preserves_same_map_structure():
+  f = MapSymbol(
+    name="f",
+  )
+
+  a = HomotopyElement(
+    name="a",
+    dimension=1,
+  )
+
+  b = HomotopyElement(
+    name="b",
+    dimension=1,
+  )
+
+  relation = Relation(
+    lhs=MapApplication(
+      map=f,
+      expression=a,
+    ),
+    rhs=MapApplication(
+      map=f,
+      expression=b,
+    ),
+    relation_type=RelationType.EQUALITY,
+  )
+
+  assert relation.lhs.map == (
+    relation.rhs.map
+  )
+
+
+def test_phase28_4_map_application_equality_distinguishes_mismatched_maps():
+  f = MapSymbol(
+    name="f",
+  )
+
+  g = MapSymbol(
+    name="g",
+  )
+
+  a = HomotopyElement(
+    name="a",
+    dimension=1,
+  )
+
+  b = HomotopyElement(
+    name="b",
+    dimension=1,
+  )
+
+  same_map_relation = Relation(
+    lhs=MapApplication(
+      map=f,
+      expression=a,
+    ),
+    rhs=MapApplication(
+      map=f,
+      expression=b,
+    ),
+    relation_type=RelationType.EQUALITY,
+  )
+
+  mismatched_map_relation = Relation(
+    lhs=MapApplication(
+      map=f,
+      expression=a,
+    ),
+    rhs=MapApplication(
+      map=g,
+      expression=b,
+    ),
+    relation_type=RelationType.EQUALITY,
+  )
+
+  assert (
+    same_map_relation
+    != mismatched_map_relation
+  )
+
+  assert (
+    mismatched_map_relation.lhs.map
+    != mismatched_map_relation.rhs.map
+  )
 
 
 
