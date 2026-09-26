bac45b5f71a1d5a5c98022196198f7b9da053337
{akebono03} <{shin8gou@gmail.com}>
2026-09-26 16:43:39 +0900

    phase143-75Q

---
+++ phase143_75q/run_phase143_75q.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = "Stop"
+
+$env:PYTHONPATH = (Get-Location).Path
+
+try {
+  python `
+    ".\phase143_75q\audit_phase143_75q_full_fallback.py"
+}
+finally {
+  Remove-Item Env:PYTHONPATH `
+    -ErrorAction SilentlyContinue
+}
