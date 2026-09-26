9e71a75d42351793c5ea6355bdbf02fb55764a26
{akebono03} <{shin8gou@gmail.com}>
2026-09-26 15:59:41 +0900

    phase143-75K

---
+++ phase143_75k/run_phase143_75k.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = "Stop"
+
+$env:PYTHONPATH = (Get-Location).Path
+
+try {
+  python `
+    ".\phase143_75k\audit_phase143_75k_finite_group_semantic_structure.py"
+}
+finally {
+  Remove-Item Env:PYTHONPATH `
+    -ErrorAction SilentlyContinue
+}
